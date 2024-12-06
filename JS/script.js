df99284dc9d53ed1ea0b5886b7077e8b646caf71
Matheus Araujo <157986764+matheusaraujo94@users.noreply.github.com>
2024-12-06 10:03:47 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+let descricaoPython = document.getElementById("descricao-python");
+let descricaoHTML = document.getElementById("descricao-html");
+let descricaoCSS = document.getElementById("descricao-css");
+
+
+function Python() {
+  descricaoPython.innerHTML =
+    "O Python é uma linguagem de programação amplamente usada em aplicações da Web, desenvolvimento de software, ciência de dados e machine learning (ML). Os desenvolvedores usam o Python porque é eficiente e fácil de aprender e pode ser executada em muitas plataformas diferentes.";
+}
+
+function HTML() {
+  descricaoHTML.innerHTML =
+    "O HTML é uma linguagem de marcação utilizada para criar estruturas na web e renderizado nos navegadores.";
+}
+
+function CSS() {
+  descricaoCSS.innerHTML =
+    "O CSS é uma linguagem de estilização utilizado para personalizar o HTML deixando mais visualmenta agradavel.";
+}
+
